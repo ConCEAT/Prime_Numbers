8a1be013efdd9ad93680d5151ac9a7ea6fd288ba
ConCEAT <diegolares@wp.pl>
2019-10-20 16:31:11 +0200

    Full test class for CheckIfPrime

--- source/Prime.py
+++ source/Prime.py
@@ -1,4 +1,5 @@
 import math
+
 def getNum(text):
     while True:
         try: num = int(input(text).strip())
@@ -9,28 +10,30 @@ def getNum(text):
         print ("Number must be positive.")
     return num
 
-def checkIfPrime(num):
-    if num < 4:
-        return not num == 1
-    if num % 6 not in [1,5]: return False
+def checkIfPrime(number):
+    if type(number) != int: raise TypeError('number must be intiger')
+    if number <= 0: raise ValueError('must be positive intiger') 
+    if number < 4:
+        return not number == 1
+    if number % 6 not in [1,5]: return False
     divisor = 5
-    while divisor <= math.sqrt(num):
-        if num % divisor == 0: return False
+    while divisor <= math.sqrt(number):
+        if number % divisor == 0: return False
         while True:
             divisor+=1
             if divisor % 6 in [1,5]: break
     return True
 
 def checkRange(start, end):
+    adjust = len(str(end))
     for num in range(start,end+1):
-        if checkIfPrime(num):
-            yield num
+        yield(('{0:%d}'%adjust).format(num), 'Prime' if checkIfPrime(num) else 'Not prime')
 
 def Main():
-    #start = getNum("Start: ")
-    #end = getNum("End: ")
-    #print(list(checkRange(start,end))[-1])
-    pass
+    start = getNum("Start: ")
+    end = getNum("End: ")
+    print (*checkRange(start,end),sep='\n')
+    
 
 if __name__ == "__main__":
 	Main()
--- test/Prime_test.py
+++ test/Prime_test.py
@@ -1,15 +1,34 @@
 import unittest
 import sys
+
 sys.path.append('..')
-from PrimeNumbers.source.Prime import checkIfPrime
+from PrimeNumbers.source import Prime
 
 class TestCheckIfPrime(unittest.TestCase):
 
     def test_primes(self):
         numbers = [2,3,5,11,19,101]
         for number in numbers:
-            with self.subTest(number = number): 
-                self.assertTrue(checkIfPrime(number))
+            with self.subTest(case = number): 
+                self.assertTrue(Prime.checkIfPrime(number))
+
+    def test_nonPrimes(self):
+        numbers = [1,4,6,25,1000,18]
+        for number in numbers:
+            with self.subTest(case = number):
+                self.assertFalse(Prime.checkIfPrime(number))
+
+    def test_values(self):
+        numbers = [0,-1,-121]
+        for number in numbers:
+            with self.subTest(case = number):
+                self.assertRaises(ValueError,Prime.checkIfPrime,number)
+    
+    def test_types(self):
+        numbers = [1.4,'4','four',2+5j,0.0,True,[]]
+        for number in numbers:
+            with self.subTest(case = number):
+                self.assertRaises(TypeError,Prime.checkIfPrime,number)
 
 
 if __name__ == '__main__':
